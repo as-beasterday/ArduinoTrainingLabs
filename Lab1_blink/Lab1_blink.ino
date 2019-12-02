de0aefaf074b36d4e0b92bab1787ee96c4fc9e53
Braeden Easterday <02braeden.easterday@gulllakecs.org>
2019-12-02 14:07:28 -0500

    Merge digital read and master

--- Lab1_blink/Lab1_blink.ino
+++ Lab1_blink/Lab1_blink.ino
@@ -3,14 +3,23 @@
  * program: blink
  * 
  * make led blink
+ * 
+ * Merge digital read
+ * programmer: me
+ * program: digital read
  */
 
-\\ iniyialize led to pin 13
+// iniyialize led to pin 13
 int led = 13;
+int pushButton = 2;
 
 void setup() {
   // using function pinMode call led provide output
   pinMode(led, OUTPUT);
+  
+  Serial.begin(9600);
+  
+  pinMode(pushButton, INPUT);
 }
 
 void loop() {
@@ -19,4 +28,8 @@ void loop() {
   delay(1500);
   digitalWrite(led, LOW);
   delay(500);
+  
+  int buttonState = digitalRead(pushButton);
+  Serial.println(buttonState);
+  delay(250);
 }
