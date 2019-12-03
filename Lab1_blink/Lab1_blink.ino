636907843fce90176f3cef1d83c3caea7e50bea3
Braeden Easterday <02braeden.easterday@gulllakecs.org>
2019-12-03 14:07:17 -0500

    Initial Potentiometer

--- Lab1_blink/Lab1_blink.ino
+++ Lab1_blink/Lab1_blink.ino
@@ -1,34 +1,18 @@
 /*
  * programmer: me
- * program: blink
+ * program: potentiometer
  * 
- * make led blink
- * 
- * Merge digital read
- * programmer: me
- * program: digital read
+ * AnalogReadSerial
+ * reads analog input on pin A0, prints to serial monitor
  */
 
-// iniyialize led to pin 13
-int led = 13;
-int pushButton = 2;
+
+
 
 void setup() {
-  // using function pinMode call led provide output
-  pinMode(led, OUTPUT);
   
-  Serial.begin(9600);
-  
-  pinMode(pushButton, INPUT);
 }
 
 void loop() {
-  int buttonState = digitalRead(pushButton);
-  Serial.println(buttonState);
-  if (buttonState==HIGH){
-    digitalWrite(led, HIGH);
-  }else{
-    digitalWrite(led, LOW);
-  }
-  delay(100);
+  
 }
