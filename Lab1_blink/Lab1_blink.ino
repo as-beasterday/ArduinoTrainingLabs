6571bfd5c6598452955696cd86b5953123a509bc
Braeden Easterday <02braeden.easterday@gulllakecs.org>
2019-12-03 14:18:42 -0500

    Working potentiometer

--- Lab1_blink/Lab1_blink.ino
+++ Lab1_blink/Lab1_blink.ino
@@ -10,9 +10,19 @@
 
 
 void setup() {
-  
+  // initialize the serial communication at 9600 bits per second
+  Serial.begin(9600);
 }
 
 void loop() {
+  // read input of analog pin 0
+  int potentiometerValue = analogRead(A0);
+
+
+  // print value of potentiometer
+  Serial.println(potentiometerValue);
+
+  // add delay for stability
+  delay(1);
   
 }
